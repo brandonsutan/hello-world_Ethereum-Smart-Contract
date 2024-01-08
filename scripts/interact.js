dd3708e0a5c5290c4a49b10f733c063d4911ba1f
brandonsutan <123422486+brandonsutan@users.noreply.github.com>
2024-01-07 19:24:43 -0500

    Add ABI generation and console log

--- scripts/interact.js
+++ scripts/interact.js
@@ -3,3 +3,9 @@
 const API_KEY = process.env.API_KEY
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
+
+// Generates an ABI and saves it to json file
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json")
+
+// Print ABI to console
+console.log(JSON.stringify(contract.abi))
