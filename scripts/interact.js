eb3c9648fbfeb1a4468ec8375317144de83cbbea
brandonsutan <123422486+brandonsutan@users.noreply.github.com>
2024-01-07 19:28:48 -0500

    Add provider, signer, and contract instantiation to interact.js

--- scripts/interact.js
+++ scripts/interact.js
@@ -9,3 +9,25 @@ const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json"
 
 // Print ABI to console
 console.log(JSON.stringify(contract.abi))
+
+// Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(
+    (network = "goerli"),
+    API_KEY
+)
+
+// Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider)
+
+// Contract
+const helloWorldContract = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    contract.abi,
+    signer
+)
+
+async function main() {
+    const message = await helloWorldContract.message()
+    console.log('The message is: "' + message)
+}
+main()
